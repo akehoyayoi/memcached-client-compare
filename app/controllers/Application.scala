aec415727d4306030d279aca0101f10ed28b0020
Yohei Okaya <okaya.dcinc@gmail.com>
2014-03-02 17:02:08 +0900

    divide composite future and individual future

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,19 +2,15 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import net.rubyeye.xmemcached.{XMemcachedClientBuilder, MemcachedClient}
-import java.io._
-import java.net._
-import java.nio.channels.AsynchronousChannelGroup
-import java.util.concurrent.Executors
+import net.rubyeye.xmemcached.XMemcachedClientBuilder
 import scala.concurrent.duration._
 import scala.concurrent.{Future, Await}
-import scala.concurrent.ExecutionContext.Implicits.global
-import akka.util.Timeout
 
 import com.klout.akkamemcached.RealMemcachedClient
 import com.klout.akkamemcached.Serialization.JBoss
 import net.rubyeye.xmemcached.utils.AddrUtil
+
+import play.api.libs.concurrent.Execution.Implicits._
 
 object Application extends Controller {
 
@@ -41,7 +37,7 @@ object Application extends Controller {
   }
 
   lazy val akkaClient = new RealMemcachedClient(List(("localhost", 11211)), 1)
-  def akka(totalRequests : Int , bodyLength : Int) = Action {
+  def akkaComposite(totalRequests : Int , bodyLength : Int) = Action {
     val memcached = akkaClient
     val listOfFutures = (1 to totalRequests).toList.map { _ =>
       val k = s"kkkkkkkkkkkkkkkk${rand.nextInt(32)}"
@@ -51,6 +47,18 @@ object Application extends Controller {
     }
     val futureList = Future.sequence(listOfFutures)
     val list = Await.result(futureList,5 seconds)
+    val s = list.flatMap(_.getOrElse("null")).mkString
+    Ok(s"length=${s.length}\n")
+  }
+
+  def akkaIndividual(totalRequests : Int , bodyLength : Int) = Action {
+    val memcached = akkaClient
+    val list = (1 to totalRequests).toList.map { _ =>
+      val k = s"kkkkkkkkkkkkkkkk${rand.nextInt(32)}"
+      val v = "v" * 1024 * bodyLength
+      memcached.set(k, v , 1 hour)
+      Await.result(memcached.get(k),5 seconds)
+    }
     val s = list.flatMap(_.getOrElse("null")).mkString
     Ok(s"length=${s.length}\n")
   }
